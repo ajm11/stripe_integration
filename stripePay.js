d649e2a3d98459aea6ede9734c923511fe76002e
Alex McLeod <alexmcleod@stripe.com>
2020-04-05 14:13:25 -0700

    visual improvements

--- stripePay.js
+++ stripePay.js
@@ -9,37 +9,45 @@ window.onload = function() {
     var penguinarray = {"leftpenguin": "sku_H2gxUY2sL8gMF2", "rightpenguin": "sku_H2ipar9VF21fKO"};
 
     leftpenguin.addEventListener('click',function(){
-        whichpenguin = "leftpenguin";
-        console.log("picked left");
-        console.log(whichpenguin);
-        console.log(penguinarray[whichpenguin]);
+        if (whichpenguin != "leftpenguin") {
+            rightpenguin.setAttribute("class","choosepenguin");
+            whichpenguin = "leftpenguin";
+            leftpenguin.setAttribute("class","choosepenguin_selected");
+        }
+        
     })
     rightpenguin.addEventListener('click',function(){
-        whichpenguin = "rightpenguin";
-        console.log("picked right");
-        console.log(whichpenguin);
-        console.log(penguinarray[whichpenguin]);
+        if (whichpenguin != "rightpenguin"){
+            leftpenguin.setAttribute("class","choosepenguin");
+            whichpenguin = "rightpenguin";
+            rightpenguin.setAttribute("class","choosepenguin_selected");
+        }
+
     })
 
     // Define the checkout button and set a listener for clicks to check out
     var checkout = document.getElementById("checkoutbutton");
     checkout.addEventListener('click', async function(){
-        console.log("In checkout " + penguinarray[whichpenguin]);
-        stripe.redirectToCheckout({
-            items: [
-              // Replace with the ID of your SKU
-              
-              //{sku: penguinarray[whichpenguin], quantity: 1}
-              {sku: penguinarray[whichpenguin], quantity: 1}
-            ],
-            successUrl: 'https://ajm11.github.io/stripe_integration/success.html',
-            cancelUrl: 'https://ajm11.github.io/stripe_integration/cancel.html',
-          }).then(function (result) {
-            // If `redirectToCheckout` fails due to a browser or network
-            // error, display the localized error message to your customer
-            // using `result.error.message`.
-            console.log(result.error.message) // ADD THIS LINE!
-          });
+        if (whichpenguin == undefined) {
+            alert("Pick a penguin in order to check out.");
+        } else{
+            console.log("In checkout " + penguinarray[whichpenguin]);
+            stripe.redirectToCheckout({
+                items: [
+                // Replace with the ID of your SKU
+                
+                //{sku: penguinarray[whichpenguin], quantity: 1}
+                {sku: penguinarray[whichpenguin], quantity: 1}
+                ],
+                successUrl: 'https://ajm11.github.io/stripe_integration/success.html',
+                cancelUrl: 'https://ajm11.github.io/stripe_integration/cancel.html',
+            }).then(function (result) {
+                // If `redirectToCheckout` fails due to a browser or network
+                // error, display the localized error message to your customer
+                // using `result.error.message`.
+                console.log(result.error.message) // ADD THIS LINE!
+            });
+        }
     })
 }
      
